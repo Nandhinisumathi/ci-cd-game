139966e7f89641c5e800daea628c402a0b9e5dd1
satheesh <satheesh.m5465@gmail.com>
2026-02-06 10:52:32 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "game_server" {
   ami           = "ami-019715e0d74f695be" 
   instance_type = "t3.micro"
-  key_name      = "jenkinskkp.pem"
+  key_name      = "jenkinskkp"
 
   security_groups = [aws_security_group.game_sg.name]
 
